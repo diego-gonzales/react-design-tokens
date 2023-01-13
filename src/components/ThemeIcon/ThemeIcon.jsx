3f888d722dbf751d1b53d57efbad1f66da81728e
Diego Gonzales <diegoegj14@gmail.com>
2023-01-13 14:54:06 -0500

    Figma screens finished

--- src/components/ThemeIcon/ThemeIcon.jsx
+++ src/components/ThemeIcon/ThemeIcon.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import "./ThemeIcon.css";
+import lunaLogo from "../../assets/luna.svg";
+import solLogo from "../../assets/sol.svg";
 
 const ThemeIcon = () => {
   const { theme, changeTheme } = useContext(ThemeContext);
@@ -14,8 +16,12 @@ const ThemeIcon = () => {
   };
 
   return (
-    <div className="theme-icon">
-      <button onClick={handleClick}>ThemeIcon</button>
+    <div className="theme-icon-container">
+      <img
+        src={theme === "light" ? lunaLogo : solLogo}
+        onClick={handleClick}
+        className="theme-icon"
+      />
     </div>
   );
 };
